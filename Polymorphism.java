7daac1c1e668d62dc852230bd837bd750b9aee0e
istiyak1264 <iamistiyakooo@gmail.com>
2025-02-04 15:47:12 +0600

    first commit

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,19 +1,18 @@
-class HouseAllot
-{
-    public void SquareFeet()
-    {
+class HouseAllot {
+    public void SquareFeet() {
         System.out.println("Square Feet depends upon LIG, MIG, HIG");
     }
 }
-class LIG extends HouseAllot
-{
-    System.out.println("LIG: Square feet is 3 cent.");
+
+class LIG extends HouseAllot {
+    public void SquareFeet() {  // Overriding method
+        System.out.println("LIG: Square feet is 3 cent.");
+    }
 }
 
-public class Polymorphism
-{
-    public static void main(String[] args)
-    {
-        
-    }    
+public class Polymorphism {
+    public static void main(String[] args) {
+        HouseAllot obj = new LIG();
+        obj.SquareFeet();
+    }
 }

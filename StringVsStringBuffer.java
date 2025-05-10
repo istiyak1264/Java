00f33355ad2c1152e5a8faeee26b6887f0c161a2
Istiyak Ahmed <iamistiyakooo@gmail.com>
2025-05-10 07:45:30 +0600

    update

--- StringVsStringBuffer.java
+++ StringVsStringBuffer.java
@@ -2,29 +2,29 @@ public class StringVsStringBuffer
 {
     public static void main(String[] args)
     {
-        int iterations = 10000;
+        int iterations = 100000;
 
         // Test String concatenation
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
         @SuppressWarnings("unused")
         String str = "";
         for (int i = 0; i < iterations; i++)
         {
             str += "a"; // String concatenation using '+'
         }
-        long endTime = System.currentTimeMillis();
-        long stringTime = endTime - startTime;
-        System.out.println("String concatenation time: " + stringTime + " ms");
+        long endTime = System.nanoTime();
+        long stringTime = (endTime - startTime) / 100000;
+        System.out.println("String concatenation time: " + stringTime + " ns");
 
         // Test StringBuffer concatenation
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         StringBuffer stringBuffer = new StringBuffer();
         for (int i = 0; i < iterations; i++)
         {
             stringBuffer.append("a"); // StringBuffer concatenation using 'append'
         }
-        endTime = System.currentTimeMillis();
-        long stringBufferTime = endTime - startTime;
-        System.out.println("StringBuffer concatenation time: " + stringBufferTime + " ms");
+        endTime = System.nanoTime();
+        long stringBufferTime = (endTime - startTime) / 100000;
+        System.out.println("StringBuffer concatenation time: " + stringBufferTime + " ns");
     }
 }
